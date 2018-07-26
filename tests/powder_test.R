e97f5585dc57c11bd1358adb2f39f8694c257d9e
Jeff Annis <jeff.annis@vanderbilt.edu>
2018-07-26 15:25:39 -0500

    rm powder_test.R

--- tests/powder_test.R
+++
@@ -1,7 +0,0 @@
-rm(list=ls())
-library(powder)
-model = LBA$new(b=T)
-data('null',package='powder')
-pow.out.null = powder(data=null,model = model,num.temps = 10,
-                        n.samples = 10,burnin=10,meltin=2)
-ml = marginal_likelihood(pow.out.null)
